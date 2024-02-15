6b0d0c8f0258faafd246cca677acb85e6565b4e8
AliTheTechKing <147366716+AliTheTechKing@users.noreply.github.com>
2024-02-15 06:53:20 +0000

    Student code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Card from './Components/Card';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Card/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
